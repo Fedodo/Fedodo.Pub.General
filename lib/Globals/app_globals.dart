55987f4db1b3f51dffa43bf3e92a4e4d5a12fe41
Lukas Nagel <lukas@lna-dev.net>
2023-06-28 07:02:15 +0200

    ✨ Added appName constant

--- lib/Globals/app_globals.dart
+++ lib/Globals/app_globals.dart
@@ -1,3 +1,4 @@
+import 'package:fedodo_general/Globals/general.dart';
 import 'package:fedodo_general/Globals/preferences.dart';
 import 'package:fedodo_general/fedodo_main.dart';
 import 'package:flutter/material.dart';
@@ -5,6 +6,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class AppGlobals {
   static void createApp(String title, Widget home) {
+    General.appName = title;
+
     WidgetsFlutterBinding.ensureInitialized();
     SharedPreferences.getInstance().then(
       // Put this into the general package
--- lib/Globals/general.dart
+++ lib/Globals/general.dart
@@ -2,6 +2,8 @@ import 'package:fedodo_general/Globals/preferences.dart';
 import 'package:jwt_decoder/jwt_decoder.dart';
 
 class General {
+  static String appName = "Fedodo";
+
   static String get fullActorId {
     return "https://${Preferences.prefs!.getString("DomainName")}/actor/$actorId";
   }
@@ -9,12 +11,12 @@ class General {
   static String get actorId {
     String? currentActorId = Preferences.prefs!.getString("CurrentActorId");
 
-    if(currentActorId == null){
+    if (currentActorId == null) {
       var temp = getActorIds().first;
       actorId = temp;
       return temp;
     }
-    
+
     return currentActorId;
   }
 
@@ -22,14 +24,14 @@ class General {
     Preferences.prefs!.setString("CurrentActorId", selectedId);
   }
 
-
-  static List<String> getActorIds(){
-    Map<String, dynamic> decodedToken = JwtDecoder.decode(Preferences.prefs!.getString(
+  static List<String> getActorIds() {
+    Map<String, dynamic> decodedToken =
+        JwtDecoder.decode(Preferences.prefs!.getString(
       "AccessToken",
     )!);
 
     String result = decodedToken["actorIds"];
-    
+
     List<String> actorIds = result.split(";");
 
     return actorIds;
